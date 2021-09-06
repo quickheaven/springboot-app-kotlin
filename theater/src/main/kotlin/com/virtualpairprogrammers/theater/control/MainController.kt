fd9c0b3f19cd545b4c11d64ae800ce5d5ae56fc0
Arjie Cristobal <ajie.io@yahoo.com>
2021-09-06 11:38:42 -0400

    Chapter 16 Getting started with Spring Boot

---
+++ theater/src/main/kotlin/com/virtualpairprogrammers/theater/control/MainController.kt
@@ -0,0 +1,54 @@
+package com.virtualpairprogrammers.theater.control
+
+import com.virtualpairprogrammers.theater.services.BookingService
+import com.virtualpairprogrammers.theater.services.TheaterService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+class MainController {
+
+    @Autowired
+    lateinit var theaterService: TheaterService // lateinit do not worry it will be initialize later on.
+
+    @Autowired
+    lateinit var bookingService: BookingService
+
+    /*
+    @RequestMapping("helloWorld")
+    fun helloWord(): ModelAndView {
+        return ModelAndView("helloWorld")
+    }
+    */
+
+    @RequestMapping("helloWorld")
+    fun helloWord(): ModelAndView = ModelAndView("helloWorld")
+
+    /*
+    fun homePage() : ModelAndView {
+        return ModelAndView("seatBooking", "bean", CheckAvailabilityBackingBean())
+    }
+     */
+    @RequestMapping("")
+    fun homePage(): ModelAndView = ModelAndView("seatBooking", "bean", CheckAvailabilityBackingBean())
+
+    @RequestMapping("checkAvailability", method = [RequestMethod.POST])
+    fun checkAvailability(bean: CheckAvailabilityBackingBean): ModelAndView {
+        val selectedSeat = theaterService.find(bean.selectedSeatNum, bean.selectedSeatRow)
+        var result = bookingService.isSeatFree(selectedSeat)
+        bean.result = "Seat $selectedSeat is " + if (result) "available" else "booked"
+        return ModelAndView("seatBooking", "bean", bean)
+    }
+}
+
+class CheckAvailabilityBackingBean {
+    val seatNums = 1..36
+    val seatRows = 'A'..'O'
+    var selectedSeatNum: Int = 1
+    var selectedSeatRow: Char = 'A'
+    var result: String = ""
+
+}
